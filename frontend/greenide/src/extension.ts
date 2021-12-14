6f959081b85c2195f69219e1d171ac1fa63800ea
GiNeXHD <73367871+GiNeXHD@users.noreply.github.com>
2021-12-14 17:55:24 +0100

    Update extension.ts

--- frontend/greenide/src/extension.ts
+++ frontend/greenide/src/extension.ts
@@ -8,33 +8,25 @@ import * as fs from 'fs';
 // Save current path of the project, it is important to be in the direct folder
 let folderPath = vscode.workspace.workspaceFolders?.map(folder => folder.uri.fsPath);
 
-const configName = "/greenideconfig.txt";
-const standardConfig = "1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1";
+const configName = "/greenide.config";
+const standardConfig = [1,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0];
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
 	console.log('folderPath: ' + folderPath);
-	fs.readFile(folderPath + configName, 'utf8', (err, data) => {
-		// check if configuration file exist
-		if(err) {
-			console.error(err.message );
-			console.log('Es wird eine neue Standradkonfiguration angelegt!');
 
-			// if configuration file does not exist, a new one will be created
-			fs.writeFile(folderPath + configName, standardConfig, err => {
-				if(err) {
-					console.error(err.message);
-				}
-				console.log('Datei wurde erfolgreich hinzugefügt!');
-			});
+	fs.readFile(folderPath + configName, function(err, data) {
+		if(err)
+		{
+			console.error(err.message);
+			fs.writeFileSync(folderPath +configName ,standardConfig.join(','));;
 		}
-
-		// if configuration file exist, it will be displayed in the console
-		console.log(data);
+		else {
+			var array = data.toString().split(',');
+			console.log(array);
+		}
 	});
-
 }
 
 // this method is called when your extension is deactivated
